87f8fa3fc083217518058cca6d640d3fe880fee7
javalover0809 <280671026.com>
2021-01-04 17:00:13 +0800

    new

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.myapplication;
+import android.os.AsyncTask;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText uname;
+    private EditText upass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        uname = (EditText) findViewById(R.id.uname);
+        upass = (EditText) findViewById(R.id.upass);
+    }
+
+    public void LoginGet(View view){
+        String name=uname.getText().toString();
+
+        String pass=upass.getText().toString();
+        System.out.println("用户名是:" + name);
+        System.out.println("密码是:" + pass);
     }
 }
